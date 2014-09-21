488023ce3fb29686f548063a53a3f4dbd4a5cab6
Matthew Whitehead <mcwhiteh@gmail.com>
2014-09-21 17:10:09 -0400

    slightly modified code and added readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-gettingCleaningData
-===================
+# Getting and Cleaning Data - Course Project
+
+##run_analysis.R accomplishes the following tasks:
+1. Merges the training and the test sets to create one data set.
+2. Extracts only the measurements on the mean and standard deviation for each measurement.
+3. Uses descriptive activity names to name the activities in the data set
+4. Appropriately labels the data set with descriptive activity names.
+5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+##to use run_analysis.R you must do the following:
+1. Download the data source and extract the files into your working directory
+2. Put ```run_analysis.R``` in your working directory
+3. Run ```source("run_analysis.R")``` to generate ```tidy_data_avg.txt``` in your working directory.
+
+## Dependencies
+data.table R library 
--- run_analysis.R
+++ run_analysis.R
@@ -8,18 +8,18 @@
 library(data.table)
 
 #read in all the data
-x_test <- read.table("./test/X_test.txt",header=FALSE)
-y_test <- read.table("./test/y_test.txt",header=FALSE)
-subject_test <- read.table("./test/subject_test.txt",header=FALSE)
-x_train <- read.table("./train/X_train.txt",header=FALSE)
-y_train <- read.table("./train/y_train.txt",header=FALSE)
-subject_train <- read.table("./train/subject_train.txt",header=FALSE)
+x_test <- read.table("./UCI HAR Dataset/test/X_test.txt",header=FALSE)
+y_test <- read.table("./UCI HAR Dataset/test/y_test.txt",header=FALSE)
+subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt",header=FALSE)
+x_train <- read.table("./UCI HAR Dataset/train/X_train.txt",header=FALSE)
+y_train <- read.table("./UCI HAR Dataset/train/y_train.txt",header=FALSE)
+subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt",header=FALSE)
 
 #get the names for the activities and features and assign those names to the test and train data
-act_names <- read.table("./activity_labels.txt",header=FALSE,colClasses="character")
+act_names <- read.table("./UCI HAR Dataset/activity_labels.txt",header=FALSE,colClasses="character")
 y_test$V1 <- factor(y_test$V1,levels = act_names$V1,labels = act_names$V2)
 y_train$V1 <- factor(y_train$V1,levels = act_names$V1,labels = act_names$V2)
-feat_names <- read.table("./features.txt",header=FALSE,colClasses="character")
+feat_names <- read.table("./UCI HAR Dataset/features.txt",header=FALSE,colClasses="character")
 headers <- feat_names[,2]
 colnames(x_test) <- feat_names$V2
 colnames(x_train) <- feat_names$V2
